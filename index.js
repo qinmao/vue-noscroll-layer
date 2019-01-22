aca2f54f65917e4266e41b70d63d8b5bbdb77716
xintairuan <1642903202@qq.com>
2019-01-22 12:20:56 +0800

    优化

--- index.js
+++ index.js
@@ -3,22 +3,20 @@ import BScroll from "better-scroll";
 (function () {
     let vNoScroll = {}
     vNoScroll.install = function (Vue) {
-        Vue.directive('no-scroll', {
-            update:function(el, binding) {
-                let show = binding.value.showLayer,
-                    parentEl = binding.value.parentEl;
-                if (show) {
-                    el.addEventListener("touchmove", function (event) {
-                        event.preventDefault();
-                    });
-                    if (parentEl) {
-                        new BScroll(document.getElementById(parentEl));
-                    }
-                } else {
-                    el.removeEventListener("touchmove", function (event) {
-                        event.preventDefault();
-                    });
+        Vue.directive('no-scroll', function (el, binding) {
+            let show = binding.value.showLayer,
+                parentEl = binding.value.parentEl;
+            if (show) {
+                el.addEventListener("touchmove", function (event) {
+                    event.preventDefault();
+                });
+                if (parentEl) {
+                    new BScroll(document.getElementById(parentEl));
                 }
+            } else {
+                el.removeEventListener("touchmove", function (event) {
+                    event.preventDefault();
+                });
             }
         })
     }
